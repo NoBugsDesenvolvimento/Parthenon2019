10803e1492ebd5414e8a5cd8ddde7cf81b704413
rogerbbatista <rogerbbatista@hotmail.com>
2019-05-09 13:49:11 -0300

    Implementação da Pesquisa - FAQ

--- app/src/main/java/com/nobugs/parthenon/Faq.java
+++ app/src/main/java/com/nobugs/parthenon/Faq.java
@@ -2,10 +2,13 @@ package com.nobugs.parthenon;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -14,22 +17,33 @@ import androidx.annotation.NonNull;
 
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 public class Faq extends Fragment {
+    private EditText search;
+    private ArrayList<View> questionsViews = new ArrayList<>();
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              final ViewGroup container,
                              Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.faq, container, false);
+        final ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.faq, container, false);
+
+        search = rootView.findViewById(R.id.search_question);
 
         LinearLayout scroll = rootView.findViewById(R.id.faq);
 
-        for (int i = 0; i < 25; i++) {
+        String[] questions = getResources().getStringArray(R.array.questions);
+        String[] answers = getResources().getStringArray(R.array.answer);
+        int total = questions.length;
+
+        for (int i = 0; i < total; i++) {
             FrameLayout templateFaq = (FrameLayout) getLayoutInflater().inflate(R.layout.faq_template, scroll, false);
+            ((TextView) templateFaq.findViewById(R.id.question_template)).setText(questions[i]);
 
             final Bundle bd = new Bundle();
-            bd.putString("title", "Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa melek, tu ta top?");
-            bd.putString("answer", getString(R.string.placeholderBig));
+            bd.putString("title", questions[i]);
+            bd.putString("answer", answers[i]);
             templateFaq.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -42,6 +56,34 @@ public class Faq extends Fragment {
 
             scroll.addView(templateFaq);
         }
+
+        rootView.findViewsWithText(questionsViews,"Question",View.FIND_VIEWS_WITH_CONTENT_DESCRIPTION);
+        search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String searchString = search.getText().toString().toLowerCase();
+
+                int total = questionsViews.size();
+                for (int i = 0; i < total; i++){
+                    String textString = ((TextView) questionsViews.get(i).findViewById(R.id.question_template)).getText().toString().toLowerCase();
+                    if (!textString.contains(searchString)){
+                        questionsViews.get(i).setVisibility(View.GONE);
+                    }else{
+                        questionsViews.get(i).setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+        });
 
         return rootView;
     }
