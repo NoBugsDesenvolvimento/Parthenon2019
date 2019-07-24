c9826a7fe05e6ca6671005efdf6ca8d4210b36db
rogerbbatista <rogerbbatista@hotmail.com>
2019-07-24 10:44:18 -0300

    Inscrição de Palestras

---
+++ project/app/src/main/java/com/nobugs/parthenon/activity/NavigationScreenAdmin.java
@@ -0,0 +1,154 @@
+package com.nobugs.parthenon.activity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
+import com.nobugs.parthenon.R;
+import com.nobugs.parthenon.fragment.CriarAtividade;
+import com.nobugs.parthenon.fragment.Duvidas;
+import com.nobugs.parthenon.fragment.Faq;
+import com.nobugs.parthenon.fragment.InscricaoHolder;
+import com.nobugs.parthenon.fragment.Profile;
+import com.nobugs.parthenon.fragment.Programacao;
+import com.nobugs.parthenon.helper.ConfiguracaoFirebase;
+
+public class NavigationScreenAdmin extends FragmentActivity {
+
+    private static final int NUM_PAGES = 4;
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
+    private BottomNavigationView navView;
+    private TabLayout tabs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_navigation_screen_adm);
+
+        viewPager = findViewById(R.id.screenPagerAdm);
+        pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+
+        tabs = findViewById(R.id.tabsAdm);
+        tabs.setupWithViewPager(viewPager);
+        tabs.addOnTabSelectedListener(changeTabListener);
+
+        navView = findViewById(R.id.nav_view_adm);
+        navView.setOnNavigationItemSelectedListener(changeNavListener);
+
+        // Chamar na Activity inicial
+        ConfiguracaoFirebase.updateValues("atividades", this);
+    }
+
+
+    private TabLayout.OnTabSelectedListener changeTabListener = new TabLayout.OnTabSelectedListener(){
+        @Override
+        public void onTabSelected(TabLayout.Tab tab) {
+            switch(tabs.getSelectedTabPosition()){
+                case 0:
+                    navView.setSelectedItemId(R.id.nav_prog_adm);
+                    break;
+                case 1:
+                    navView.setSelectedItemId(R.id.nav_noticias_adm);
+                    break;
+                case 2:
+                    navView.setSelectedItemId(R.id.nav_faq_adm);
+                    break;
+                case 3:
+                    navView.setSelectedItemId(R.id.nav_perfil_adm);
+                    break;
+            }
+        }
+        @Override
+        public void onTabUnselected(TabLayout.Tab tab) {
+
+        }
+
+        @Override
+        public void onTabReselected(TabLayout.Tab tab) {
+
+        }
+    };
+
+    private BottomNavigationView.OnNavigationItemSelectedListener changeNavListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.nav_prog_adm:
+                    viewPager.setCurrentItem(0);
+                    return true;
+                case R.id.nav_noticias_adm:
+                    viewPager.setCurrentItem(1);
+                    return true;
+                case R.id.nav_faq_adm:
+                    viewPager.setCurrentItem(2);
+                    return true;
+                case R.id.nav_perfil_adm:
+                    viewPager.setCurrentItem(3);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+
+    //Acho isso aqui meio bobo
+    /*@Override
+    public void onBackPressed() {
+        if (viewPager.getCurrentItem() == 0) {
+            super.onBackPressed();
+        } else {
+            viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
+        }
+    }*/
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        private ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    return new CriarAtividade();
+                case 1:
+                    return new Faq();
+                case 2:
+                    return new Duvidas();
+                case 3:
+                    return new Profile();
+            }
+            return new Faq();
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return ""+position;
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+}
--- project/app/src/main/java/com/nobugs/parthenon/fragment/CriarAtividade.java
+++ project/app/src/main/java/com/nobugs/parthenon/fragment/CriarAtividade.java
@@ -1,12 +1,12 @@
 package com.nobugs.parthenon.fragment;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import com.google.firebase.database.DatabaseReference;
@@ -25,26 +25,64 @@ public class CriarAtividade extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_criar_atividade, container, false);
+        final ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_criar_atividade, container, false);
+
+        rootView.findViewById(R.id.btCriar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                confirmar(rootView);
+            }
+        });
+
+
+        return rootView;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+
+
+    }
 
     public void confirmar(View v){
-        String titulo = v.findViewById(R.id.etTitulo).toString();
-        String sumario = v.findViewById(R.id.etSumario).toString();
-        String palestrante = v.findViewById(R.id.etPalestrante).toString();
-        String local = v.findViewById(R.id.etLocal).toString();
-        int vagas = Integer.parseInt(v.findViewById(R.id.etVagas).toString());
-        String data = v.findViewById(R.id.etData).toString();
-        String hora_inicial = v.findViewById(R.id.etHoraI).toString();
-        String hora_final = v.findViewById(R.id.etHoraF).toString();
+        String titulo = ((EditText) v.findViewById(R.id.etTitulo)).getText().toString();
+        String sumario = ((EditText) v.findViewById(R.id.etSumario)).getText().toString();
+        String palestrante = ((EditText) v.findViewById(R.id.etPalestrante)).getText().toString();
+        String local = ((EditText) v.findViewById(R.id.etLocal)).getText().toString();
+        String str_vagas = ((EditText) v.findViewById(R.id.etVagas)).getText().toString();
+        int vagas = 0;
+        if (!str_vagas.equals(""))
+            vagas = Integer.parseInt(str_vagas);
+        String data = ((EditText) v.findViewById(R.id.etData)).getText().toString();
+        String hora_inicial = ((EditText) v.findViewById(R.id.etHoraI)).getText().toString();
+        String hora_final = ((EditText) v.findViewById(R.id.etHoraF)).getText().toString();
+
+        String empty = "";
+        if (titulo.equals(""))
+            empty += "\n- Título";
+        if (palestrante.equals(""))
+            empty += "\n- Palestrante(s)";
+        if (data.equals(""))
+            empty += "\n- Data";
+        if (hora_inicial.equals(""))
+            empty += "\n- Hora (início)";
+
+        if (!empty.equals("")) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+            builder.setTitle(R.string.title_erro_criacao)
+                    .setMessage(empty);
+            AlertDialog dialog = builder.create();
+            dialog.show();
+            return;
+        }
 
         String latlng = "não sei pegar ainda";
 
         AtividadesAux atvAux = new AtividadesAux(titulo, sumario, 1, local, data, hora_inicial, hora_final, vagas, "", latlng);
 
         FirebaseDatabase db = ConfiguracaoFirebase.getDatabase();
-        DatabaseReference myRef = db.getReference("atividades/");
+        DatabaseReference myRef = db.getReference("atividades/").push();
         myRef.setValue(atvAux);
 
         String key = myRef.getKey();
@@ -54,5 +92,21 @@ public class CriarAtividade extends Fragment {
         RealmHelper.startTransaction();
         realm.insertOrUpdate(atv);
         RealmHelper.endTransaction();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle(R.string.title_confirma_criacao)
+                .setMessage("   ");
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        ((EditText) v.findViewById(R.id.etTitulo)).setText("");
+        ((EditText) v.findViewById(R.id.etSumario)).setText("");
+        ((EditText) v.findViewById(R.id.etPalestrante)).setText("");
+        ((EditText) v.findViewById(R.id.etLocal)).setText("");
+        ((EditText) v.findViewById(R.id.etVagas)).setText("");
+        ((EditText) v.findViewById(R.id.etData)).setText("");
+        ((EditText) v.findViewById(R.id.etHoraI)).setText("");
+        ((EditText) v.findViewById(R.id.etHoraF)).setText("");
+
     }
 }
